6db1b5f583a0a4e2a1cff88c273453fe8bce0c54
Patgar1 <mateusz789852@gmail.com>
2024-08-23 00:05:33 +0200

    Add download handling

---
+++ download-button-handler.js
@@ -0,0 +1,3 @@
+document.getElementById('button').addEventListener('click', () => {
+    window.location.href = 'https://github.com/OpenATBP/OpenATBPClient/releases/download/1.0/OpenATBPClient-Installer-1.0.0.exe';
+});
