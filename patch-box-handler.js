aedd7db16154cc2ac43bad87676512910ef2ac24
Patgar1 <mateusz789852@gmail.com>
2024-10-05 18:31:09 +0200

    Improve the styling of the patch notes page

--- patch-box-handler.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('patch-notes-section').addEventListener('click', function(event) {
-    if (event.target.closest('.patch-box')) {
-        const patchBox = event.target.closest('.patch-box');
-        const id = patchBox.id;
-        window.location.href = "patch-notes/" + id + ".html";
-    }
-});
