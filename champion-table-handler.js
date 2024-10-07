22a3bad9687471655aa40781b00b941d2f02d121
Patgar1 <mateusz789852@gmail.com>
2024-10-07 14:18:02 +0200

    Improve mobile readability for most pages

--- champion-table-handler.js
+++ champion-table-handler.js
@@ -521,6 +521,21 @@ window.onload = function() {
             }
         }
     }
+
+    // add mobile nav handler here cause two window.onload scripts cant be used on one page
+    const mobileNavOpenBtn = document.getElementById('mobile-nav-open-btn');
+    const mobileNavCloseBtn = document.getElementById('mobile-nav-close');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    mobileNavOpenBtn.addEventListener('click', function() {
+        mobileNav.style.left = '0';
+        mobileNavOpenBtn.style.display = 'none';
+    });
+
+    mobileNavCloseBtn.addEventListener('click', function() {
+        mobileNav.style.left = '-100%';
+        mobileNavOpenBtn.style.display = 'flex';
+    });
 }
 
 const selectElement = document.querySelector('.champion-level-select');
--- item-stat-handler.js
+++ item-stat-handler.js
@@ -208,4 +208,19 @@ window.onload = function() {
             }
         }
     }
+
+    // add mobile nav handler here cause two window.onload scripts cant be used on one page
+    const mobileNavOpenBtn = document.getElementById('mobile-nav-open-btn');
+    const mobileNavCloseBtn = document.getElementById('mobile-nav-close');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    mobileNavOpenBtn.addEventListener('click', function() {
+        mobileNav.style.left = '0';
+        mobileNavOpenBtn.style.display = 'none';
+    });
+
+    mobileNavCloseBtn.addEventListener('click', function() {
+        mobileNav.style.left = '-100%';
+        mobileNavOpenBtn.style.display = 'flex';
+    });
 }
