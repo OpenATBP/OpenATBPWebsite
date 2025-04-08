142f759a881d3e583bfab19684e5665ebae1ebbc
Kowi21 <mateusz.p.wieckowski@gmail.com>
2025-04-08 06:40:16 +0200

    Add download link for macOS

--- download-button-handler.js
+++ download-button-handler.js
@@ -1,3 +1,8 @@
-document.getElementById('button').addEventListener('click', () => {
-    window.location.href = 'https://github.com/OpenATBP/OpenATBPClient/releases/download/1.0/OpenATBPClient-Installer-1.0.0.exe';
-});
+const WINDOWS_DOWNLOAD_URL = "https://github.com/OpenATBP/OpenATBPClient/releases/download/1.1/OpenATBPClient-Windows-Installer-1.1.0.exe";
+const MAC_DOWNLOAD_URL = "https://github.com/OpenATBP/OpenATBPClient/releases/download/1.1/OpenATBPClient-MacOS-1.1.0.dmg";
+
+const windowsDownloadBtn = document.getElementById("windows-download");
+const macDownloadBtn = document.getElementById("mac-download");
+
+if (windowsDownloadBtn) windowsDownloadBtn.href = WINDOWS_DOWNLOAD_URL;
+if (macDownloadBtn) macDownloadBtn.href = MAC_DOWNLOAD_URL;
